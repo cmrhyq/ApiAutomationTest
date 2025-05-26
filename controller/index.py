c4c847ea8036249cef0666c6284077a317f087bc
cmrhyq <cmrhyq@163.com>
2025-05-26 10:30:49 +0800

    创建数据库

--- controller/index.py
+++ controller/index.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 index_bp = Blueprint('index', __name__)
 
-@index_bp.route('/index', methods=['GET', 'POST'])
-def index():
-    return "hello world"
+@index_bp.route('/', methods=['GET', 'POST'])
+def index(name):
+    return f"hello world, {name}"
