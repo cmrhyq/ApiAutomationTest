f918ae7af60b86f8d5bb3f258e372a05cb233744
cmrhyq <cmrhyq@163.com>
2025-05-26 14:34:59 +0800

    安装axios

--- web/electron/main.ts
+++ web/electron/main.ts
@@ -48,7 +48,7 @@ function createWindow() {
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
+// for application and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
