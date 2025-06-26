380a88748dbe78c0e88b9baa945940e0a7d387e3
cmrhyq <cmrhyq@163.com>
2025-06-26 16:55:38 +0800

    1. 新增tabs slice

--- api-auto-test-web/src/store/index.ts
+++ api-auto-test-web/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './slices/userSlice';
+import userReducer from './reducers/user/userSlice';
+import tabsReducer from "./reducers/tabs/tabsSlice.ts";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    tabs: tabsReducer,
   },
 });
 
